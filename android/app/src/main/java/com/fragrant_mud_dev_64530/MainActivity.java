e4930610dd880b88e16f04a0677e1fac426c172c
Crowdbotics <engineering@crowdbotics.com>
2023-02-20 00:10:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fragrant_mud_dev_64530/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fragrant_mud_dev_64530;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fragrant_mud_dev_64530";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-mud-dev-64530",
+  url: "https://fragrant-mud-dev-64530.botics.co",
+  api: "https://fragrant-mud-dev-64530.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
